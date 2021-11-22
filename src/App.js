2d64a346b53e9a3162b0a2877f180eb82ba9fc33
Schow94 <Schow94@gmail.com>
2021-11-22 10:32:54 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+
+// const URL = `https://cors-anywhere.herokuapp.com`;
+// const API_URL = `${URL}/https://zccschow.zendesk.com/api/v2/tickets`;
+const API_URL = `http://localhost:5000`;
+
+const App = () => {
+	const getData = async () => {
+		const res = await axios.get(API_URL);
+		const tickets = res.data.tickets;
+		console.log("DATA: ", tickets);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (
+		<div className="App">
+			<div>
+				<h1>This is the App component</h1>
+			</div>
+		</div>
+	);
+};
 
 export default App;
